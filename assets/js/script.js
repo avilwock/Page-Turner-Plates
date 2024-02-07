abd3c46a16f3319442c3496e3692fbef49072bd3
DannyM15 <merinodanny15@gmail.com>
2024-02-06 19:54:31 -0800

    APIs working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 var recSearch = document.querySelector('#query');
-// API food Key = dfc30cdf3a8c48e292b7870da115a1ce
-var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=dfc30cdf3a8c48e292b7870da115a1ce&query=${recSearch.value}`
 
-//book API key npg4qc8fzyzb793s57jf4v2w - Key not active yet. 
-var bookURL = `https://reststop.randomhouse.com/resources/titles?start=0&max=4&expandLevel=0&onsaleStart=MM/dd/yyyy&onsaleEnd=MM/dd/yyyy&authorid=0&workid=0&keyword=Grisham%20Christmas`
+//Food recipe search API
+var recipeURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recSearch.value}`
 
+//Book Search API key npg4qc8fzyzb793s57jf4v2w -
+var bookURL = `https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/search?q=${recSearch.value}&api_key=npg4qc8fzyzb793s57jf4v2w
+`
 
+// Fetch function to pull recipes from API
 function searchbtn(event) {
     event.preventDefault();
-    fetch(recipeURL)
-    .then(res => res.json())
-    .then(recList => {
-        console.log(recList)
-        console.log(recSearch.value)
-    })
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recSearch.value}`)
+        .then(res => res.json())
+        .then(recList => {
+            console.log(recList)
+            console.log(recSearch.value)
+        })
 }
 
+// Fetch Funtion to pull books from API
+function bookSearch(event) {
+    event.preventDefault();
+    fetch(`https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/search?q=${recSearch.value}&api_key=npg4qc8fzyzb793s57jf4v2w
+    `)
+    
+        .then(res => res.json())
+        .then(bookList => {
+            console.log(bookList)
+            console.log(recSearch.value)
+        })
+}
 var searchButton = document.querySelector("#fetch-button");
 
+searchButton.addEventListener("click", bookSearch);
 searchButton.addEventListener("click", searchbtn);
